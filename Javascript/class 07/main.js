8d050beeb6778eb763662db1537aa43f561c580d
Tech Soft Engineer <techmazeem@gmail.com>
2026-06-25 12:31:01 +0500

    class 07

---
+++ Javascript/class 07/main.js
@@ -0,0 +1,96 @@
+console.log("class 07");
+
+// ============= Arrays in JavaScript ===========
+
+// let stu1 = "ALi";
+// let stu2 = "ALi";
+// let stu3 = "ALi";
+// let stu4 = "ALi";
+// let stu5 = "ALi";
+
+// Array
+// let students = [ "Ali" , "Ahmad" , "Danial" , "Bilal" , "Arham" ];
+// // console.log(students.length);
+// // console.log(students[0],students[1],);
+// // console.log(students[2]);
+
+// for(let i = 0 ; i< students.length ; i++ ){
+
+//     console.log(students[i]);
+
+// }
+
+// Practice 01
+
+// average = (20 + 30 + 40 + 70 + 90)/5
+
+// let marks = [ 85 , 40 , 79 , 20 , 60 , 90];
+
+// let sum = 0;
+// for(let i = 0 ; i< marks.length ; i++){
+
+//     sum = sum + marks[i] ;
+
+// }
+// // console.log(sum);
+// let avg = sum / marks.length;
+
+// console.log(`The Average marks is ${avg}`);
+
+
+// practice 02
+
+// let items = [250 , 540 , 900 , 180 , 1080];
+// let per = prompt("Enter the discounted value:");
+
+// for(let a = 0; a< items.length ; a++){
+//     // console.log(items[a]);
+//     let offer = ( items[a] * per )/100;
+//     // console.log(offer);
+//     items[a] = items[a] - offer ;
+//     // console.log(items[a] );
+
+// }
+
+// console.log(items);
+
+
+//  Arrays Methods
+
+// let user = 50;
+// let num = [30 , 60 , 89 , 67 , 45 , 87];
+// let arr = [ 3, 4];
+
+// num.push(user, 40);
+;
+
+// console.log(num.pop());
+// console.log(num.concat(arr).toString());
+
+// num.unshift(2);
+// num.shift();
+
+// console.log(num.slice(1 , 5));
+// delete
+// num.splice(2 , 1 )
+// add
+// num.splice(4 , 0 , 2 )
+// replace
+// num.splice(1 , 1 , 70 );
+// console.log(num);
+
+// practice 03
+let companies = ["Microsoft", "infnix" , "Tecno" , "vivo" , "iphone" , "sumsung"];
+
+// companies.shift();
+// companies.splice(2, 1 , "oppo");
+companies.push("Realme")
+console.log(companies);
+
+
+
+
+
+
+
+
