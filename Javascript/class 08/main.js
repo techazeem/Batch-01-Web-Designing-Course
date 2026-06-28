a2f99be3eb830521d153a2e81954331e8037ba7e
Tech Soft Engineer <techmazeem@gmail.com>
2026-06-28 11:00:46 +0500

    js class 08

---
+++ Javascript/class 08/main.js
@@ -0,0 +1,92 @@
+console.log("class 08");
+
+// ============== Functions =============
+
+// console.log();
+
+// function printText(text ){
+//     // console.log("pakistan");
+//     // console.log("I Love Js");
+//     // console.log("This is the javaScript Function");
+//     console.log(`i love ${text} `);
+    
+    
+// }
+
+// printText("Pakistan"); //pakistan
+// printText("javascript");
+
+// function sum(num1 , num2){
+//     // console.log(`sum is ${num1 + num2}`);
+//     console.log("hello");
+//     return num1 + num2;
+    
+    
+// }
+
+
+
+// let res = sum(10 , 40 );
+// console.log(res);
+
+// sum(30 , 70 );
+
+
+// ======== Arrow Function
+
+ 
+// const arrow = (s)=>{
+//     console.log("pakistan" , s);
+    
+//         // return a + b;
+//      }
+
+//      arrow(20);
+// Practice 01
+
+// function countVowel(text){
+//     let count = 0; //3
+//     for(let val of text){
+        
+//         // console.log(val);
+//         if(val === "a" || val === "e" || val === "i" || val === "o" || val === "u"){
+//             count++;
+//         }
+        
+//     }
+//     console.log(`The ${text} include the ${count} vowels letter`);
+    
+// }
+ 
+//  countVowel("yriwoiuytrweiskjhaio");
+
+//  let cVowel = (text)=>{
+//      let count = 0; //3
+//     for(let val of text){
+        
+//         // console.log(val);
+//         if(val === "a" || val === "e" || val === "i" || val === "o" || val === "u"){
+//             count++;
+//         }
+        
+//     }
+//     console.log(`The ${text} include the ${count} vowels letter`);
+//  }
+
+//  cVowel("i love pakistan")
+
+// let arr = [ 2 ,4, 5, 7, 8,9 ];
+
+arr.forEach( (num)=>{
+    console.log(num *2);
+    
+})
+
+// practice 03
+
+let array1 = [20 , 4, 70 , 45 , 30];
+
+array1.forEach( (num)=>{
+    console.log(num**2);
+});
+
